c1d4438297def474de716e03fa112470f698ce96
PChemGuy <39730837+pchemguy@users.noreply.github.com>
2025-12-14 17:22:36 +0300

    Update snippets.sql

--- src/ilsvrc2012_devkit/snippets.sql
+++ src/ilsvrc2012_devkit/snippets.sql
@@ -5,6 +5,21 @@ FROM synsets, definitions
 WHERE synsets.rowid = definitions.synset_rowid;
 
 
+-- SELECT synsets
+
+WITH
+    synsets_ex AS (
+        SELECT
+			rowid,
+            id AS omwid,
+			metadata ->> '$.identifier' AS sid,
+            metadata ->> '$.imagenet' AS imagenet_id
+        FROM synsets
+		WHERE pos = 'n'
+    )
+SELECT * FROM synsets_ex
+
+
 -- SELECT words and headword (sense synset_rank=0)
 
 WITH
@@ -16,6 +31,7 @@ WITH
             forms.rowid AS form_rowid,
             forms.form,
             synsets.id AS omwid,
+            substr(synsets.metadata ->> '$.identifier', 1, length(synsets.metadata ->> '$.identifier') - 5) AS headword,
             synsets.metadata AS synset_metadata
         FROM senses, forms, synsets
         WHERE senses.entry_rowid = forms.entry_rowid
@@ -25,7 +41,7 @@ WITH
         SELECT
             synset_rowid,
             omwid,
-            substr(synset_metadata ->> '$.identifier', 1, length(synset_metadata ->> '$.identifier') - 5) AS headword,
+            headword,
             json_group_array(form ORDER BY synset_rank) AS words,
             synset_metadata
         FROM synsets_forms    
@@ -51,3 +67,49 @@ FROM relation_types AS rt
 LEFT JOIN sense_relations AS sr
 ON rt.rowid = sr.type_rowid
 GROUP BY rt.rowid;
+
+
+-- Region related
+
+WITH
+    synsets_ex AS (
+        SELECT
+			rowid AS rid,
+            id AS omwid,
+			metadata ->> '$.identifier' AS sid,
+            metadata ->> '$.imagenet' AS imagenet_id
+        FROM synsets
+		WHERE pos = 'n'
+    ),
+	related AS (
+		SELECT src.rid, src.omwid, src.sid, dst.sid AS rel_sid
+		FROM synsets_ex AS src, synsets_ex AS dst, synset_relations AS sr, relation_types AS rt
+		WHERE rt.type = 'has_domain_region'
+		  AND rt.rowid = sr.type_rowid
+		  AND sr.source_rowid = src.rid
+		  AND sr.target_rowid = dst.rid		  
+	)
+SELECT * FROM related;
+
+
+-- Topic related
+
+WITH
+    synsets_ex AS (
+        SELECT
+			rowid AS rid,
+            id AS omwid,
+			metadata ->> '$.identifier' AS sid,
+            metadata ->> '$.imagenet' AS imagenet_id
+        FROM synsets
+		WHERE pos = 'n'
+    ),
+	related AS (
+		SELECT src.rid AS src_rid, src.omwid, src.sid, dst.rid AS rel_rid, dst.sid AS rel_sid
+		FROM synsets_ex AS src, synsets_ex AS dst, synset_relations AS sr, relation_types AS rt
+		WHERE rt.type = 'has_domain_topic'
+		  AND rt.rowid = sr.type_rowid
+		  AND sr.source_rowid = src.rid
+		  AND sr.target_rowid = dst.rid		  
+	)
+SELECT * FROM related
